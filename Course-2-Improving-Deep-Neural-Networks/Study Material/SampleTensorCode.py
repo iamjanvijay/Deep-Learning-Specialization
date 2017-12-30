e530d9f4500491b118bf2e48f1f2328c2dceb877
mejanvijay <janvijay.singhcd.cse14@itbhu.ac.in>
2017-12-30 22:57:42 +0530

    30-Dec

--- Course-2-Improving-Deep-Neural-Networks/Study Material/SampleTensorCode.py
+++ Course-2-Improving-Deep-Neural-Networks/Study Material/SampleTensorCode.py
@@ -113,6 +113,33 @@ tf.sigmoid(...)
 tf.softmax(...)
 sess.run(..., feed_dict = {x: z})
 
+# For convolutional Neural Nets : 
+
+tf.nn.conv2d(X,W1, strides = [1,s,s,1], padding = 'SAME')
+# Given an input  X  and a group of filters  W1 , this function convolves  W1 's filters on X. 
+# The third input ([1,f,f,1]) represents the strides for each dimension of the input (m, n_H_prev, n_W_prev, n_C_prev)
+
+tf.nn.max_pool(A, ksize = [1,f,f,1], strides = [1,s,s,1], padding = 'SAME')
+# Given an input A, this function uses a window of size (f, f) and strides of size (s, s) to carry out max pooling over each window. 
+
+tf.nn.relu(Z1)
+# Computes the elementwise ReLU of Z1 (which can be any shape). 
+
+tf.contrib.layers.flatten(P)
+# Given an input P, this function flattens each example into a 1D vector it while maintaining the batch-size. 
+# It returns a flattened tensor with shape [batch_size, k].
+
+tf.contrib.layers.fully_connected(F, num_outputs)
+# Given a the flattened input F, it returns the output computed using a fully connected layer.
+
+tf.nn.softmax_cross_entropy_with_logits(logits = Z3, labels = Y)
+# computes the softmax entropy loss. 
+# This function both computes the softmax activation function as well as the resulting loss.
+
+tf.reduce_mean 
+# computes the mean of elements across dimensions of a tensor
+# Use this to sum the losses over all the examples to get the overall cost. 
+
 
 
 
